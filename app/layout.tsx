ae354eb0edf6ceb2adfb8b219876f6cdad59b811
Senzso <your-email@example.com>
2025-01-07 01:24:44 +0100

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      <link rel="icon" href="/icon.png" />
+
         <Script src="https://cdn.jsdelivr.net/npm/@solana/web3.js@latest" strategy="beforeInteractive" />
       </head>
       <body className="min-h-screen bg-black text-white overflow-x-hidden">
